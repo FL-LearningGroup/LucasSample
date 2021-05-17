70843ba3f8d450ded8c669bf1ac7cf8543b307ff
Lucas Yao <v-diya@microsoft.com>
2021-05-17 14:03:03 +0800

    init and generate NetGenericHost.csproj.

---
+++ net/net-generic-host/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetGenericHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
